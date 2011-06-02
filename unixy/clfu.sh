ad3f6d3d443e16c8414d0a54b451ba797da51140
Huy Z <huy-git@huyzing.com>
2011-06-02 23:40:50 +1000

    clfu: added

---
+++ unixy/clfu.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Filename:         clfu.sh
+# Version:          0.1
+# Description:
+#   Quick-reference for the top 100 commands at commandlinefu.com
+#
+# Author:           Huy Z, http://huyzing.com/
+# Created on:       2010-10-16
+#
+# Usage:            just run it
+
+curl -s \
+  www.commandlinefu.com/commands/browse/sort-by-votes/plaintext \
+  www.commandlinefu.com/commands/browse/sort-by-votes/plaintext/25 \
+  www.commandlinefu.com/commands/browse/sort-by-votes/plaintext/50 \
+  www.commandlinefu.com/commands/browse/sort-by-votes/plaintext/75 \
+| grep -v "commandlinefu.com by" | nl -b 'p^ *#' -n ln | less -F
