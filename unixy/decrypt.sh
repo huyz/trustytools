933a73f29c128a89cd6b97bce01b6095d428c073
huyz <h-git@huyz.us>
2013-09-11 16:50:29 -0700

    Decrypt: only prompt once for passphrase

--- unixy/decrypt.sh
+++ unixy/decrypt.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # $RCSfile: decrypt,v $ $Revision: 1.7 $ $Date: 2011/05/19 08:11:10 $
-# Encrypts specified files
-# 2011-05-18 Handles all types of encrypted files
+# Decrypts specified files, prompting for passphrase only once
 
 if [ $# -eq 0 ]; then
   echo "Usage: $0 file..." >&2
@@ -13,13 +12,18 @@ if ! which gpg >/dev/null 2>&1; then
   exit 1
 fi
 
+echo "Enter passphrase: \c"
+stty -echo
+read passphrase
+stty echo
+
 for i in "$@"; do
   case "$i" in
     *.3des|*.gpg)
       [ $# -gt 0 ] && echo "=== $i"
       out="${i%.gpg}"
       out="${out%.3des}"
-      gpg -d -o "$out" "$i" && touch -r "$i" "$out"
+      echo "$passphrase" | gpg --passphrase-fd 0 --batch -q -d -o "$out" "$i" && touch -r "$i" "$out"
       ;;
     *)
       echo "WARNING: skipping $i; must be named *.3des" >&2
