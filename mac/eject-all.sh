d8608f3535117b89c5b4548032ff7a8cd220f869
huyz <huyz@users.noreply.github.com>
2020-12-07 01:47:22 +0700

    mac/eject-all.sh: Create

---
+++ mac/eject-all.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Source: https://apple.stackexchange.com/a/394594/6278
+
+#script to eject all external drives
+disks=$(diskutil list external | sed -n '/[Ss]cheme/s/.*B *//p')
+
+if [ "$disks" ]; then
+    echo "$disks" | while read line ; do
+        diskutil unmountDisk "/dev/$line"
+    done
+else
+    echo "No external disks to eject."
+fi
