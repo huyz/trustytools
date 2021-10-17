4c1fe82388c20d2c386929cc594fd7d9ecaf5ba8
huyz <huyz@users.noreply.github.com>
2021-10-17 23:41:33 +0700

    mac/mobilesync-list: skip directories without Info.plist

--- mac/mobilesync-list.sh
+++ mac/mobilesync-list.sh
@@ -13,6 +13,12 @@ cd "$DIR"
 
 ls -1dt ./[0-9a-f]*/. | while read -r i; do
   i="$(dirname "$i")"
+  if [[ ! -e "$i/Info.plist" ]]; then
+      # Allow user to create Info.plist.is.missing to stop the warnings
+      [[ ! -e "$i/Info.plist.is.missing" ]] &&
+        echo "error: Can't find file $i/Info.plist" >&2
+      continue
+  fi
   name="$(plutil -p "$i/Info.plist" | sed -n 's/.*Device Name.*"\(.*\)"$/\1/p')"
   date="$(stat -x "$i/Info.plist" 2>/dev/null | sed -n 's/^Modify:[[:space:]]*//p')"
   echo "$i|$name|$date"
