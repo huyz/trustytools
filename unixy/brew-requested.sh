85051b44a0e59957539b205ef72232189f7d5689
huyz <huyz@users.noreply.github.com>
2023-03-03 22:11:53 -0800

    brew-requested: create

---
+++ unixy/brew-requested.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Lists all requested packagse
+
+# Source:
+#   https://apple.stackexchange.com/questions/412352/list-all-homebrew-packages-explicitly-installed-by-the-user-without-deps/438632#438632
+brew info --json=v2 --installed \
+    | jq -r '.formulae[] | select(any(.installed[]; .installed_on_request)).full_name'
