6c9ad2175f3a23d32fa783dc405030cd8ef7bcd2
huyz <huyz@users.noreply.github.com>
2023-03-29 06:01:44 -0700

    eject-all: uuse terminal-notifier if available

--- mac/eject-all.sh
+++ mac/eject-all.sh
@@ -1,13 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 # Source: https://apple.stackexchange.com/a/394594/6278
+# Prerequisites: brew install terminal-notifier
+
+set -euo pipefail
+shopt -s failglob
+trap 'echo "ERR signal on line $(caller)" >&2' ERR
+trap exit INT
+export PS4='+(BASH_SOURCE:LINENO): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+#### Utils
+
+function notify {
+    echo "$*"
+    if [[ ! -t 1 ]]; then
+        command -v terminal-notifier &>/dev/null &&
+            terminal-notifier -title eject-all -message "$*"
+    fi
+}
+
+#### Main
 
 #script to eject all external drives
 disks=$(diskutil list external | sed -n '/[Ss]cheme/s/.*B *//p')
 
 if [ "$disks" ]; then
+    fail=0
     echo "$disks" | while read -r line ; do
-        diskutil unmountDisk "/dev/$line"
+        diskutil unmountDisk "/dev/$line" || (( fail++ ))
     done
+    if (( fail > 0 )); then
+        notify "❌ Failed to eject $fail disk(s)."
+        exit 1
+    else
+        notify "✅ Done."
+    fi
 else
-    echo "No external disks to eject."
+    notify "✅ No disks to eject."
 fi
