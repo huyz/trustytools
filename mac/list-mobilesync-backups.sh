c600e8b7e89468871d6e5315cd137fc91eedeb8f
huyz <huyz@users.noreply.github.com>
2023-08-04 05:11:25 -0700

    list-mobilesync-backups: display size and volume

--- mac/list-mobilesync-backups.sh
+++ mac/list-mobilesync-backups.sh
@@ -7,19 +7,54 @@ shopt -s failglob
 trap exit INT
 
 
+#### Config
+
 DIR=~/"Library/Application Support/MobileSync/Backup"
+
+#### Args
+
+opt_verbose=
+if [[ $# -gt 0 ]]; then
+    case "$1" in
+        -v) opt_verbose=opt_verbose ;;
+        *)
+            echo "Usage: $0 [-v]" >&2
+            exit 1
+            ;;
+    esac
+fi
+
+#### Main
 
 cd "$DIR"
 
-ls -1dt ./[0-9a-f]*/. | while read -r i; do
-    i="$(dirname "$i")"
-    if [[ ! -e "$i/Info.plist" ]]; then
-        # Allow user to create Info.plist.is.missing to stop the warnings
-        [[ ! -e "$i/Info.plist.is.missing" ]] &&
-            echo "error: Can't find file $i/Info.plist" >&2
-        continue
+(
+    if [[ -n $opt_verbose ]]; then
+        echo "Directory|Device|Date|Size|Volume"
+        echo "---------|------|----|----|------"
+    else
+        echo "Directory|Device|Date|Volume"
+        echo "---------|------|----|------"
     fi
-    name="$(plutil -p "$i/Info.plist" | sed -n 's/.*Device Name.*"\(.*\)"$/\1/p')"
-    date="$(stat -x "$i/Info.plist" 2>/dev/null | sed -n 's/^Modify:[[:space:]]*//p')"
-    echo "$i|$name|$date"
-done | column -t -s '|'
+
+    # shellcheck disable=SC2012
+    ls -1dt ./[0-9a-f]*/. | while read -r i; do
+        i="$(dirname "$i")"
+        if [[ ! -e "$i/Info.plist" ]]; then
+            # Allow user to create Info.plist.is.missing to stop the warnings
+            [[ ! -e "$i/Info.plist.is.missing" ]] &&
+                echo "error: Can't find file $i/Info.plist" >&2
+            continue
+        fi
+        name="$(plutil -p "$i/Info.plist" | sed -n 's/.*Device Name.*"\(.*\)"$/\1/p')"
+        date="$(TZ= stat -f "%Sm" -t "%Y-%m-%d %H:%MZ" "$i/Info.plist" 2>/dev/null)"
+        vol="$(df "$i" | sed -n '2s,/Volumes/,,;2s/.*%[[:space:]]*//p')"
+        printf "%s|%s|%s" "$i" "$name" "$date"
+
+        if [[ -n $opt_verbose ]]; then
+            size="$(du -shH "$i" | cut -f1)"
+            printf "|%s" "$size"
+        fi
+        printf "|%s\n" "$vol"
+    done
+) | column -t -s '|'
