40b2778f53352d8266750f780173ee5c2728f53f
huyz <huyz@users.noreply.github.com>
2023-08-27 01:42:22 -0700

    minor

--- unixy/brew-deptree.sh
+++ unixy/brew-deptree.sh
@@ -5,4 +5,4 @@ brew leaves --installed-on-request \
     | xargs brew deps --installed --skip-recommended --include-requirements --include-build --include-optional --annotate --tree \
     | sed -E "s/^[[:alnum:]\/_-]+$/$(tput setaf 4)&$(tput sgr0)/" \
     | sed -E "s/^.* \[build\]$/$(tput setaf 2)&$(tput sgr0)/" \
-    | sed -E "s/^.* \[optional\]$/$(tput setaf 3)&$(tput sgr0)/" \
+    | sed -E "s/^.* \[optional\]$/$(tput setaf 3)&$(tput sgr0)/"
