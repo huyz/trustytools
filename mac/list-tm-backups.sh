f15c08ae9adb9da7014ee938c5b4a836832ee895
huyz <huyz@users.noreply.github.com>
2023-10-08 13:46:26 -0700

    list-tm-backups: color-code timestamps

--- mac/list-tm-backups.sh
+++ mac/list-tm-backups.sh
@@ -4,7 +4,7 @@
 # For readarray
 [ "${BASH_VERSINFO:-0}" -ge 4 ] || { echo "${BASH_SOURCE[0]}: Error: bash v4+ required." >&2; exit 1; }
 
-#### Preamble (template v2023-01-31)
+#### Preamble (v2023-10-01)
 
 set -euo pipefail
 shopt -s failglob
@@ -12,18 +12,35 @@ shopt -s failglob
 function trap_err { echo "ERR signal on line $(caller)" >&2; }
 trap trap_err ERR
 trap exit INT
-export PS4='+(BASH_SOURCE:LINENO): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
-if [[ $EUID -eq 0 ]]; then
-    [ -x "${GETOPT:=/opt/local/bin/getopt}" ] || \
-        { echo "$0: Error: \`sudo port install util-linux\` to install $GETOPT." >&2; exit 1; }
+if [[ $OSTYPE == darwin* ]]; then
+    if [[ "${EUID:-$UID}" -eq 0 ]]; then
+        [ -x "${REALPATH:=/opt/local/bin/grealpath}" ] || \
+            { echo "$0: Error: \`sudo port install coreutils\` to install $REALPATH." >&2; exit 1; }
+        [ -x "${DATE:=/opt/local/bin/gdate}" ] || \
+            { echo "$0: Error: \`sudo port install coreutils\` to install $DATE." >&2; exit 1; }
+        [ -x "${GETOPT:=/opt/local/bin/getopt}" ] || \
+            { echo "$0: Error: \`sudo port install util-linux\` to install $GETOPT." >&2; exit 1; }
+    else
+        HOMEBREW_PREFIX="$( (/opt/homebrew/bin/brew --prefix || /usr/local/bin/brew --prefix || brew --prefix) 2>/dev/null)"
+        [ -x "${REALPATH:="$HOMEBREW_PREFIX/bin/grealpath"}" ] || \
+            { echo "$0: Error: \`brew install coreutils\` to install $REALPATH." >&2; exit 1; }
+        [ -x "${DATE:="$HOMEBREW_PREFIX/bin/gdate"}" ] || \
+            { echo "$0: Error: \`brew install coreutils\` to install $DATE." >&2; exit 1; }
+        [ -x "${GETOPT:="$HOMEBREW_PREFIX/opt/gnu-getopt/bin/getopt"}" ] || \
+            { echo "$0: Error: \`brew install gnu-getopt\` to install $GETOPT." >&2; exit 1; }
+    fi
 else
-    HOMEBREW_PREFIX="$( (/opt/homebrew/bin/brew --prefix || /usr/local/bin/brew --prefix || brew --prefix) 2>/dev/null)"
-    [ -x "${GETOPT:="$HOMEBREW_PREFIX/opt/util-linux/bin/getopt"}" ] || \
-        { echo "$0: Error: \`brew install util-linux\` to install $GETOPT." >&2; exit 1; }
+    HOMEBREW_PREFIX="$( (/home/linuxbrew/.linuxbrew/bin/brew --prefix || brew --prefix) 2>/dev/null)"
+    REALPATH="realpath"
+    DATE="date"
+    GETOPT="getopt"
 fi
 
 SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
+#SCRIPT="$($REALPATH "${BASH_SOURCE[0]}")"
+#SCRIPT_DIR="$(dirname "$SCRIPT")"
 
 #### Options
 
@@ -55,7 +72,25 @@ done
 
 [[ $# -eq 0 ]] || usage
 
+##############################################################################
+#### Util
 
+function colored_icon {
+    local icon timestamp_ts="$1"
+
+    if [[ "$timestamp_ts" -gt "$($DATE -d '36 hours ago' +%s)" ]]; then
+        icon="🟢"
+    elif [[ "$timestamp_ts" -gt "$($DATE -d '7 days ago' +%s)" ]]; then
+        icon="🟡"
+    elif [[ "$timestamp_ts" -gt "$($DATE -d '30 days ago' +%s)" ]]; then
+        icon="🟠"
+    else
+        icon="🔴"
+    fi
+    echo "$icon"
+}
+
+##############################################################################
 #### Main
 
 # Force network backup mount (necessary if your TM backups are through the network)
@@ -72,9 +107,16 @@ fi
 for mountpoint in "${destinations[@]}"; do
     echo "--- ${mountpoint/\/Volumes\//} ---"
     (tmutil listbackups -d "$mountpoint" -m || true) | while read -r backup; do
-        time="$(sed -n 's,.*/\(.*\)\.backup$,\1,p' <<<"$backup")"
+        # Convert from weird format to ISO 8601
+        timestamp="$(sed -n 's,.*/\(.*\)\.backup$,\1,p' <<<"$backup" \
+            | perl -pe 's/^(\d{4}-\d{2}-\d{2})-(\d{2})(\d{2})(\d{2})$/$1T$2:$3/')"
+        timestamp_ts="$(TZ=America/Los_Angeles $DATE -d "$timestamp" +%s)"
+        # Remove the `:` between hour and minute to save space
+        timestamp="$(TZ=America/Los_Angeles $DATE -d "$timestamp" +%FT%H%M)"
+
         size="$(tmutil uniquesize "$backup" | awk '{print $1}')"
-        printf "%s %8s\n" "$time" "$size"
+
+        printf "%s %s %7s\n" "$(colored_icon "$timestamp_ts")" "$timestamp" "$size"
     done | tail -n "${opt_lines:-99999}"
     echo
 done
