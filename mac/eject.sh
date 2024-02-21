dd55ef8b5cc751e9620035c7f47f29d1f15a7d44
huyz <huyz@users.noreply.github.com>
2024-02-20 23:18:21 -0800

    eject: support "-a" for entire disk; refactor

--- mac/eject.sh
+++ mac/eject.sh
@@ -1,25 +1,38 @@
-#!/bin/sh
-# Filename:         osxeject.sh
-# Version:          0.1
-# Description:
-#   On OS X, eject a removable disk by user-friendly volume name
-#
-# Platforms:        OS X
-# Source:           https://github.com/huyz/trustytools
-# Author:           Huy Z, http://huyz.us/
-# Created on:       2011-01-23
-#
-# Usage:
-#   osxeject "volume name" ...
+#!/bin/bash
+# Usage: eject [-a] VOLUME…
+#   -a to eject all the other volumes on the same physical disks of the specified VOLUMEs
 
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 \"Volume Name\" ..."
+set -euo pipefail
+shopt -s failglob
+# shellcheck disable=SC2317
+function trap_err { echo "ERR signal on line $(caller)" >&2; }
+trap trap_err ERR
+trap exit INT
+export PS4='+(BASH_SOURCE:LINENO): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
+
+#### Arguments
+
+if [[ $# -eq 0 ]]; then
+    echo "Usage: $SCRIPT_NAME VOLUME…" >&2
     exit 1
 fi
 
-for i in "$@"; do
-    for j in $(mount | sed -n "s,^\\(/dev/[^ 	]*\\)[ 	][ 	]*on[ 	][ 	]*/Volumes/${i}[ 	]*.*,\\1,p"); do
-        echo hdiutil eject "$j"
-        hdiutil eject "$j"
+opt_all=
+if [[ $1 == -a ]]; then
+    shift
+    opt_all=1
+fi
+
+#### Main
+
+for name in "$@"; do
+    for dev in $(mount | sed -n "s,^\\(/dev/[^[:space:]]*\\)[[:space:]][[:space:]]*on[[:space:]][[:space:]]*/Volumes/${name} (.*),\\1,p"); do
+        if [[ -n $opt_all ]]; then
+            dev="${dev%s?}"
+        fi
+        echo hdiutil eject "$dev"
+        hdiutil eject "$dev"
     done
 done
