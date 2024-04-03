3f90b82a008a3f57f35e96626f2896ee4fb3c89c
huyz <huyz@users.noreply.github.com>
2024-04-03 07:54:11 -0700

    autoclear-clipboard: check for proper password regex

--- mac/autoclear-clipboard.sh
+++ mac/autoclear-clipboard.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
 # 2020-06-27 Automatically clears the pasteboard if it contains a password-looking for too long.
-# Counter-measure against Universal Pasteboard, which is dangerous as iOS apps will
-# often read the pasteboard when they launch (e.g, TikTok)
+# Counter-measure against Apple's Universal Pasteboard, which is dangerous as iOS apps will
+# often read the pasteboard when they launch (e.g, TikTok).
+#
+# WARNING: clipboard managers such as Paste will still record them in the history.
+#
+# --- Testing ---
+# To test:
+#   - `lunchy stop com.example.autoclear-clipboard``
+#   - Change CHECK_INTERVAL from 12 to 2
+#   - `~/bin/autoclear-clipboard``
+#
+# Postive cases:
+#   PAssword!@12345
+#   P/Assword-12345
+#   Password@12345
+#   PAss!123
+# Negative cases:
+#   PAssword -12345                         # Has space
+#   PAssword	-12345                      # Has tab
+#   PAss!12                                 # Too short
+#   PAssword!@12345PAssword!@12345678       # Too long
+#   /PAssword-12345                         # Starts with slash
+#   http://googl3.com/                      # Starts with http
+#   file://localhost:0/                       # Starts with file
+#   screenshot.2023-04-12T141507Z           # Starts with screenshot
+
+#### Preamble (v2024-01-14)
+
+set -euo pipefail
+shopt -s failglob extglob
+# shellcheck disable=SC2317
+function trap_err { echo "ERR signal on line $(caller)" >&2; }
+trap trap_err ERR
+trap exit INT  # So that ^C will stop the entire script, not just the current subprocess
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
 
 ### Config
 
 DEBUG=
 
 # In seconds
-CHECK_INTERVAL=15
+CHECK_INTERVAL=12
+# To debug:
+#CHECK_INTERVAL=2
 
 # If set to 1, only clears output that is password-like: a specific length and no whitespace and doesn't start
 # with http
@@ -39,7 +75,24 @@ while true; do
         prev_content="$content"
         prev_seconds=$SECONDS
 
-    elif [[ ${#content} -ge 8 && ${#content} -le 30 && "${content// /}" == "$content" && "$content" != http* ]]; then
+    # What looks like a password:
+    # - length: 8 to 32, inclusive
+    # - no whitespace
+    # - doesn't start with common prefixes
+    # - doesn't look like a path
+    # - and looks like a decently-formatted password:
+    #   - two or more lowercase letters
+    #   - one or more uppercase letters
+    #   - one or more digits
+    #   - one or more of the following special characters: !@#$%^&*-
+    #   Based on https://stackoverflow.com/questions/4670639/regex-match-a-strong-password-with-two-or-more-special-characters/4670743#4670743
+    elif [[ ${#content} -ge 8 && ${#content} -le 32 \
+            && "$content" != *\ * \
+            && "$content" != *$'\t'* \
+            && ! "$content" =~ ^\.?/[-_/[:alnum:]]+$ \
+            && "$content" != @(http|file|screenshot)* ]] \
+        && perl -lne 'exit(! /^(?=(?:.*[a-z]){2})(?=(?:.*[A-Z]){1})(?=(?:.*\d){1})(?=(?:.*[!@#$%^&*-]){1}).{8,}$/);' <<<"$content"; then
+
         [[ -n $DEBUG ]] && echo "New password. Storing..." >&2
         prev_content="$content"
         prev_seconds=$SECONDS
