9987c744c401f1b2955cce8149a4a467d5e32f24
huyz <huyz@users.noreply.github.com>
2024-04-24 12:42:21 -0700

    cmpdir: improve output

--- unixy/cmpdir.sh
+++ unixy/cmpdir.sh
@@ -61,52 +61,55 @@ fi
 # Must end in slash to compare the contents of the directory
 src="${src%%/}/"
 
+##############################################################################
+### Util
+
+# shellcheck disable=SC2120
+hr () {
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "${1:--}"
+}
+
 ### Display instructions
 
+hr
 cat <<'END'
-=== Legend
+𐄫 Legend
 
 The output shows a code detailing the differences for each file or directory
 that differs. There is no output if they are the same. The code has columns
 `YXcstpoguax` where each character is a dot `.` if that aspect of the comparison
 is ok, or a letter:
 
-```
-Y is type of update:
-   < sent (not appropriate in this case)
-   > need to copy
-   c missing file or directory
-   h is hard link
-   . no update
-   * and rest of line is a message, eg *deleting
-X file type: f file  d dir  L symlink  D device S special file
-c checksum differs. + new item  " " same
-s size differs
-t timestamp differs
-p permissions differ
-o owner differ
-g group differ
-u (not used)
-a acl differ
-x extended attributes differ
-```
+    Y is type of update:
+    < sent (not appropriate in this case)
+    > need to copy
+    c missing file or directory
+    h is hard link
+    . no update
+    * and rest of line is a message, eg *deleting
+    X file type: f file  d dir  L symlink  D device S special file
+    c checksum differs. + new item  " " same
+    s size differs
+    t timestamp differs
+    p permissions differ
+    o owner differ
+    g group differ
+    u (not used)
+    a acl differ
+    x extended attributes differ
 
 For example,
 
-```
-.d..t...... a/b/                    directory timestamp differs
-cL+++++++++ a/b/d -> /nosuch2       symbolic link missing
-cS+++++++++ a/b/f                   special file missing (a/b/f is a fifo)
->f..t...... a/b/ff                  file timestamp differs
-hf          a/b/xx1 => a/b/xx       files should be a hard linked
-cLc.t...... a/b/z -> /tmp/hi2       symbolic link to different name
-cd+++++++++ a/c/                    directory missing
->f+++++++++ a/c/i.10                missing file needs to be copied
-
-
-=== Log
-
+    .d..t...... a/b/                    directory timestamp differs
+    cL+++++++++ a/b/d -> /nosuch2       symbolic link missing
+    cS+++++++++ a/b/f                   special file missing (a/b/f is a fifo)
+    >f..t...... a/b/ff                  file timestamp differs
+    hf          a/b/xx1 => a/b/xx       files should be a hard linked
+    cLc.t...... a/b/z -> /tmp/hi2       symbolic link to different name
+    cd+++++++++ a/c/                    directory missing
+    >f+++++++++ a/c/i.10                missing file needs to be copied
 END
+hr
 
 
 ### Log output and error
@@ -115,15 +118,15 @@ END
 # NOTE: macOS mktemp requires XXXXXXXX to be at the end.
 tmpfile="$(mktemp "${TMP:-/tmp}/$(basename "$0").log.XXXXXXX")"
 
-echo "NOTE: for standard output and error:  less $tmpfile"
-echo
-echo "=== rsync"
-echo
+cat <<EOF
 
-#cleanup() {
-#  [ -e "$tmpfile" ] && rm -f "$tmpfile"
-#}
-#trap cleanup HUP INT QUIT TERM EXIT
+𐄫 Log file
+
+For standard output and error: less "$tmpfile"
+
+𐄫 Executing \`rsync\`…
+
+EOF
 
 # Redirect stdout `>` into a named pipe `>(…)` running `tee`
 exec &> >(tee "$tmpfile")
@@ -131,4 +134,15 @@ exec &> >(tee "$tmpfile")
 ### Execute
 
 set -x
-exec rsync -ni -aH ${opt_checksum:+"$opt_checksum"} ${opt_delete:+"$opt_delete"} --info=progress2 --exclude='.git' --exclude='.idea' --exclude='.mypy_cache' "$src" "$tar"
+exec rsync -ni -aH \
+    ${opt_checksum:+"$opt_checksum"} \
+    ${opt_delete:+"$opt_delete"} \
+    --info=progress2 \
+    --exclude='.git' \
+    --exclude='.idea' \
+    --exclude='.mypy_cache' \
+    --exclude='.venv' \
+    --exclude='.vscode' \
+    --exclude='node_modules' \
+    --exclude='venv' \
+    "$src" "$tar"
