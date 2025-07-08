00efcbad22811a4162a6889261554b0e4159ea4d
huyz <huyz@users.noreply.github.com>
2025-07-07 21:32:38 -0700

    explain: update for python3

--- contrib/explain.py
+++ contrib/explain.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
-import urllib
+import urllib.parse
 import webbrowser
 
-url = "http://explainshell.com/explain?cmd=" + urllib.quote(' '.join(sys.argv[1:]))
+url = "http://explainshell.com/explain?cmd=" + urllib.parse.quote(' '.join(sys.argv[1:]))
 webbrowser.open_new(url)
